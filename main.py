5bba69b6229dc5e65510db8f0c0de444485d92fe
qiShang-cc <73888648+qiShang-cc@users.noreply.github.com>
2025-03-12 11:21:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ with open("README.md", "r") as f:
     # if last_update_date == current_date:
         # sys.exit("Already updated today!")
 
-keywords = ["Time Series", "Trajectory", "Graph Neural Networks"] # TODO add more keywords
+keywords = ["Kalman Filter", "MIMO radar multi-target tracking", "Partical Filter", "Multi-sensor data fusion"] # TODO add more keywords
 
 max_result = 100 # maximum query results from arXiv API for each keyword
 issues_result = 15 # maximum papers to be included in the issue
